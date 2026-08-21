236a3e721bbb4eb1161a8a96fb6b05e43455071a
Sneha Das <sd5747399@gmail.com>
2026-08-21 04:19:52 +0000

    Revert "Add import for urlConfig in DetailsPage component"
    This reverts commit 7aaf16e693a4a550896ad32a2d4697073260a75d.

--- giftlink-frontend/src/components/DetailsPage/DetailsPage.js
+++ giftlink-frontend/src/components/DetailsPage/DetailsPage.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import './DetailsPage.css';
-import { urlConfig } from '../../config';
 
 function DetailsPage() {
     const navigate = useNavigate();
